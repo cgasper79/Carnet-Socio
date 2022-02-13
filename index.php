14456604bb75e1408d5fc7f22f5e39f6869da47f
cgasper79 <51058790+cgasper79@users.noreply.github.com>
2022-02-13 22:59:43 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Meta Requerido-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    
+    <!-- CSS Personalizado -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Socio Minimal</title>
+  </head>
+
+<body>
+  <div class="wrapper fadeInDown">
+    <div id="formContent">
+      <!-- Tabs Titles -->
+  
+      <!-- Icon -->
+      <div class="fadeIn first">
+        <img src="./assets/img/Logo_gris.png" id="icon" alt="User Icon" />
+        <h2> Carnet de Socio</h2>
+      </div>
+  
+      <!-- Login Form -->
+      <form>
+        <input type="text" id="numSocio" class="fadeIn second" name="numSocio" placeholder="Número de socio">
+        <input type="text" id="apellido" class="fadeIn third" name="apellido" placeholder="Primer Apellido">
+        <input type="submit" class="fadeIn fourth" value="Obtener">
+      </form>
+  
+      <div class="credits fadeIn fourth">
+        <a href="https://gasperwebdesign.com/"><img src="./assets/img/logo_gasper.png" id="icon" alt="User Icon" /></a>
+        <p>&copy; 2022 Diseño</p>
+      </div>
+
+    </div>
+    
+  </div>
+
+   
+
+    <!-- Bootstrap 4 JS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
